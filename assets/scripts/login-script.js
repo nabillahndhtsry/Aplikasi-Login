0d42bea2a3cdb90f256214f2117edd8a37ae00cf
Nabillah Indah Tsuraya <nabillahtrsy412@gmail.com>
2026-02-16 17:45:32 +0700

    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,33 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* comment : Membuat variable untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* comment : membuat variable untuk menyimpan informasi email dan passwornd. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* comment : mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /* comment : memastikan bahwan nilai email dan password sesuai dengan nilai yang tersimpan. */
+    if (email === expectedEmail && password === expectedPassword) {
+      /* comment : jika sesuai makan program akan berpindah ke halaman home. */
+      goToHome();
+    } else {
+
+      /* comment : namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+      showPopUp();
+    }
 });
